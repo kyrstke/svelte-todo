308201595d92a6c0d8c3241dc738ca30e89b6b77
Bambo <kyrstke@gmail.com>
2023-06-23 10:10:58 +0200

    Add Todo component

---
+++ client/src/components/Todo.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let _id: string;
+	export let text: string;
+	export let done: boolean;
+
+	const dispatch = createEventDispatcher();
+
+	function handleCheckboxChange() {
+		dispatch('checkboxChange', { _id, text, done });
+	}
+
+	function handleDelete() {
+		dispatch('delete', { _id });
+	}
+
+</script>
+
+<div class="flex justify-between items-center">
+	<p class="ml-2 text-lg font-medium" class:done>{text}</p>
+    <div class="flex items-center gap-2">
+        <input type="checkbox" bind:checked={done} on:change={handleCheckboxChange} />
+	    <button class="ml-auto material-symbols-outlined" on:click={handleDelete}>delete</button>
+    </div>
+</div>
+
+<style>
+    .done {
+        text-decoration: line-through;
+    }
+</style>

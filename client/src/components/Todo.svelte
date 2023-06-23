956f1d4168d9193f7b8d23838b52bc0c8b991865
Bambo <kyrstke@gmail.com>
2023-06-23 10:52:56 +0200

    Add adding todos feature

--- client/src/components/Todo.svelte
+++ client/src/components/Todo.svelte
@@ -15,18 +15,19 @@
 		dispatch('delete', { _id });
 	}
 
+	
 </script>
 
 <div class="flex justify-between items-center">
 	<p class="ml-2 text-lg font-medium" class:done>{text}</p>
-    <div class="flex items-center gap-2">
-        <input type="checkbox" bind:checked={done} on:change={handleCheckboxChange} />
-	    <button class="ml-auto material-symbols-outlined" on:click={handleDelete}>delete</button>
-    </div>
+	<div class="flex items-center gap-2">
+		<input type="checkbox" bind:checked={done} on:change={handleCheckboxChange} />
+		<button class="ml-auto material-symbols-outlined" on:click={handleDelete}>delete</button>
+	</div>
 </div>
 
 <style>
-    .done {
-        text-decoration: line-through;
-    }
+	.done {
+		text-decoration: line-through;
+	}
 </style>
--- client/src/components/TodoList.svelte
+++ client/src/components/TodoList.svelte
@@ -15,7 +15,10 @@
 		console.log(todos);
 	}
 
-	onMount(fetchTodos);
+    async function handleNewTodo(event: CustomEvent<ResponseAPI>) {
+        const newTodo = event.detail;
+        todos = [...todos, newTodo];
+    }
 
 	async function handleCheckboxChange(event: CustomEvent<ResponseAPI>) {
 		const todoToUpdate = event.detail;
@@ -36,6 +39,31 @@
 		const updatedTodos = todos.filter((todo) => todo._id !== todoToDelete._id);
 		todos = updatedTodos;
 	}
+
+    async function handleSubmit(event: Event) {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+        const input = form.elements.namedItem('text') as HTMLInputElement;
+        const inputValue = input.value.trim();
+        if (!inputValue) {
+            alert('Please enter a todo item');
+            return;
+        }
+        const newTodo = {
+            text: inputValue,
+            done: false
+        };
+        try {
+            const response = await client.post('', newTodo);
+            todos = [...todos, response.data];
+            input.value = '';
+        } catch (error) {
+            console.error(error);
+            alert('Error creating todo');
+        }
+    }
+
+    onMount(fetchTodos);
 </script>
 
 <h1 class="text-5xl font-bold mb-10">{title}</h1>
@@ -57,3 +85,8 @@
 		{/if}
 	{/each}
 </div>
+
+<form on:submit={handleSubmit}>
+	<input type="text" name="text" placeholder="Add a new todo..." class="rounded p-2" />
+	<button type="submit" class="button">Add</button>
+</form>
